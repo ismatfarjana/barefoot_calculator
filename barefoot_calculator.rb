f8070a3b4d0a4321e4a9b586282a2d65245a1999
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2019-12-16 22:44:37 +1100

    Add details.

--- barefoot_calculator.rb
+++ barefoot_calculator.rb
@@ -8,7 +8,7 @@ require 'colorize'
 puts "         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:light_green)
 puts "        |                                                                             |".colorize(:light_green)
 print "        |".colorize(:light_green)  
-print "                           BUDGET CALCULATOR                                 ".colorize(:black).on_light_cyan
+print "                          BAREFOOT BUDGET CALCULATOR                         ".colorize(:black).on_light_cyan
 puts "|".colorize(:light_green)      
 #puts "        |                                                                             |".colorize(:light_green)
 
@@ -28,6 +28,28 @@ print "Manage Your money in Futureproof way".colorize(:light_yellow).underline
 puts "                      |".colorize(:light_green)
 print "        | ".colorize(:light_green) 
 puts "                                                                            |".colorize(:light_green)
+
+####  ASKING NAME #####
+print "        |                       ".light_green 
+print " Good day mate!!".light_white
+puts "                                      |".light_green
+sleep 0.5
+
+print "        |                        ".light_green
+print "What's your name?".light_white
+puts "                                    |".light_green
+
+print "                         MY NAME IS > ".light_cyan
+name = gets.chomp.split(" ").map {|x| x.capitalize}.join(" ")
+first_name = (name.split(" ")).first
+
+puts "        |                                                                             |".colorize(:light_green)
+
+print "               ".light_green
+print "                #{first_name}, Glad to meet you!".light_white
+puts "                               ".light_green
+
+
 
 print "        | ".colorize(:light_green)  
 print "                I am here to help you manage your money.".colorize(:light_white) 
@@ -61,13 +83,25 @@ if answer == "Y"
     puts "        |                                                                             |".colorize(:light_green)
 
     print "                         " 
-    print "Monthly income: $".colorize(:green).underline
+    print "Monthly income: $".colorize(:green)
     income = gets.chomp.to_f.round(2) 
 
     puts "        |                                                                             |".colorize(:light_green)
-    print "        |".colorize(:light_green)
-    print "                    Calculation finished..   " .colorize(:light_white)
+    print "        |                   ".colorize(:light_green)
+    print "Initalizing Calculation .." .colorize(:light_white).underline
     puts "                                |".colorize(:light_green)
+                sleep 1
+    puts "        |                                                                             |".colorize(:light_green)
+    print "        |".colorize(:light_green)
+    print "                         Calculation Complete !!!" .colorize(:light_white)
+    puts "                            |".colorize(:light_green)
+             sleep 1
+    puts "        |                                                                             |".colorize(:light_green)         
+
+    print "        |                         ".colorize(:light_green)
+    print "Here is your BUDGET:".light_cyan.underline
+    puts "                                |".light_green
+             sleep 1
 
 
 #################  calculation #############
@@ -84,18 +118,61 @@ if answer == "Y"
     
     puts "        |                                                                             |".colorize(:light_green)
     puts "        |                                                                             |".colorize(:light_green)
-    print "        |                      ".colorize(:light_green)
-    puts " Daily expence  = $#{daily_expence} ".black.on_yellow
+   # print "        |                      ".colorize(:light_green)
+    print "                           "
+    puts " Daily expence in total     = $#{daily_expence}  ".black.on_yellow
     
 
-    print "        |                      ".colorize(:light_green)
-    puts " Splurge        = $#{splurge}  ".colorize(:color => :black, :background => :yellow)
+   # print "        |                      ".colorize(:light_green)
+    print "                           "
+    puts " Splurge(short-term savings)=  $#{splurge}  ".colorize(:color => :black, :background => :yellow)
 
-    print "        |                      ".colorize(:light_green)
-    puts " Smile          = $#{smile}  ".colorize(:color => :black, :background => :yellow)
+    #print "        |                      ".colorize(:light_green)
+    print "                           "
+    puts " Smile(long-term savings)   =  $#{smile}  ".colorize(:color => :black, :background => :yellow)
 
-    print "        |                      ".colorize(:light_green)
-    puts " Fire Ext       = $#{fire_extinguisher}  ".colorize(:color => :black, :background => :yellow)
+    #print "        |                      ".colorize(:light_green)
+    print "                           "
+    puts " Fire Ext(emmergency money) = $#{fire_extinguisher}  ".colorize(:color => :black, :background => :yellow) 
+    puts "        |                                                                             |".colorize(:light_green) 
+
+
+    sleep 0.5
+
+    print "                          ".light_green
+    print " After ONE year, you will have $#{(splurge*12)}".green
+    puts "            ".light_green
+    print "        |               ".light_green
+    print " to spend on HOLIDAYS or anything you want.".green
+    puts "                   |".light_green
+
+sleep 0.5
+
+    puts "        |                                                                             |".colorize(:light_green) 
+
+    print "                           ".light_green
+    print " After TEN years, you will save $#{(smile*120)}  ".yellow
+    puts "                  ".light_green
+    print "        |                         ".light_green
+    print " for your longterm goal.".yellow
+    puts "                            |".light_green
+
+sleep 0.5
+
+    puts "        |                                                                             |".colorize(:light_green) 
+
+    print "                          ".light_green
+    print " And each years, you will have $#{(fire_extinguisher*12)}  ".light_red
+    puts "                  ".light_green
+    print "        |             ".light_green
+    print " as bonus saving if no emergency situation happens .".light_red
+    puts "            |".light_green
+
+
+
+
+
+
 
 
     puts "        |                                                                             |".colorize(:light_green)
@@ -126,8 +203,13 @@ else answer == "N"
     
 end    
 
-
-puts "        |                                                                             |".colorize(:light_green)
+print "        |".light_green
+print "*****************************************************************************".red
+puts "|".light_green
+#puts "        |                                                                             |".colorize(:light_green)
+print "        |".colorize(:light_green)
+print "                   DEVELOPED BY:  SYEDA ISAMAT FARJANA                       ".black.on_light_blue
+puts "|".colorize(:light_green)
 puts "         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:light_green)
 
 
